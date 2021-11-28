4d4e90b4c5f3106167d0aedafc092f5377833a3b
dalilaoliva80@gmail.com <dalilaoliva80@gmail.com>
2021-11-27 19:06:08 -0500

    Another test

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-import sys
+#import sys
 
-<<<<<<< HEAD
 #Hello I am just testing if this works.
-=======
 #Testing this inside GitHub
->>>>>>> c5062a10904142127b845291f68fd1392792d75e
+
+print("Hello")
+name=input("Name:")
+print(name)
