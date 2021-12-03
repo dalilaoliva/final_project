296af26f7444c945b12b6427db74f488fca0d649
dalilaoliva80@gmail.com <dalilaoliva80@gmail.com>
2021-12-02 21:41:56 -0500

    getting changes

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 
 app = Flask(__name__)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
