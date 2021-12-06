cffd46d83ec68aa7372cdccea3d3d4f7b618ee6b
cncarrascoz <celestecarrasco@college.harvard.edu>
2021-12-06 12:57:04 -0500

    trying to get tutorials to work

--- app.py
+++ app.py
@@ -144,6 +144,20 @@ def sorting():
         return render_template("home.html", number=number)
     return render_template("quiz.html")
 
+@app.route("/tutorials2")
+def sorting2():
+    workout_style = db.execute("SELECT workout_style FROM users")
+    if workout_style == 1 or 2 or 3:
+        homeup = db.execute("SELECT name, link FROM videos WHERE exercise_type = homeup")
+        homelow = db.execute("SELECT name, link FROM videos WHERE exercise_type = homelow")
+        return render_template("tutorials2.html", homeup = homeup, homelow = homelow)
+    elif workout_style == 4 or 5 or 6:
+        gymup = db.execute("SELECT name, link FROM videos WHERE exercise_type = gymup")
+        gymlow = db.execute("SELECT name, link FROM videos WHERE exercise_type = gymlow")
+        return render_template("tutorials2.html", gymup = gymup, gymlow = gymlow)
+    else:
+        return render_template("tutorials2.html")
+
 @app.route("/home", methods=["GET", "POST"])
 def workout_page():
     style = db.execute("SELECT workout_style FROM users WHERE id = ?", session['user_id'])
