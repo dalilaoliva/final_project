ab936936d6e7c5358e64b9eec70388b0c489b731
cncarrascoz <celestecarrasco@college.harvard.edu>
2021-12-06 13:48:29 -0500

    tutorials changes

--- app.py
+++ app.py
@@ -148,15 +148,15 @@ def sorting():
 def sorting2():
     workout_style = db.execute("SELECT workout_style FROM users")
     if workout_style == 1 or 2 or 3:
-        homeup = db.execute("SELECT name, link FROM videos WHERE exercise_type = homeup")
-        homelow = db.execute("SELECT name, link FROM videos WHERE exercise_type = homelow")
-        return render_template("tutorials2.html", homeup = homeup, homelow = homelow)
+        homeup = db.execute("SELECT name, link FROM videos WHERE exercise_type = 'homeup' ")
+        homelow = db.execute("SELECT name, link FROM videos WHERE exercise_type = 'homelow' ")
+        return render_template("tutorials2.html", homeup = homeup, homelow = homelow, workout_style = workout_style)
     elif workout_style == 4 or 5 or 6:
-        gymup = db.execute("SELECT name, link FROM videos WHERE exercise_type = gymup")
-        gymlow = db.execute("SELECT name, link FROM videos WHERE exercise_type = gymlow")
-        return render_template("tutorials2.html", gymup = gymup, gymlow = gymlow)
+        gymup = db.execute("SELECT name, link FROM videos WHERE exercise_type = 'gymup' ")
+        gymlow = db.execute("SELECT name, link FROM videos WHERE exercise_type = 'gymlow' ")
+        return render_template("tutorials2.html", gymup = gymup, gymlow = gymlow, workout_style = workout_style)
     else:
-        return render_template("tutorials2.html")
+        return render_template("/")
 
 @app.route("/home", methods=["GET", "POST"])
 def workout_page():
