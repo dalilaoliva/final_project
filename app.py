a525d3751d789082cdaecde6aedce6bdb4510da7
dalilaoliva80@gmail.com <dalilaoliva80@gmail.com>
2021-12-07 01:01:35 -0500

    changes

--- app.py
+++ app.py
@@ -134,9 +134,14 @@ def sorting():
     number=0
     if request.method == "POST":
 
-        if not request.form["option1"] or not request.form["option2"]:
-            return apology("Must choose one answer from each question", 400)
-            
+        print("inside of the post")
+        if not request.form.get("option1"):
+            print("reuqest 1")
+            return apology("Choose one answer from each question", 403)
+        if not request.form.get("option2"):
+            print("reuqest 2")
+            return apology("Choose one answer from each question", 403)
+
         print (request.form["option1"])
         print (request.form["option2"])
         if request.form["option1"] == "home":
